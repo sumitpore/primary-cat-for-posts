453ab470c3ff70c63e55c4d0eb9ad37d1761d510
Sumit Pore <sumit.pore@wisdmlabs.com>
2019-03-14 01:38:18 +0530

    Change post type to taxonomy

--- admin/class-pcp-admin-settings.php
+++ admin/class-pcp-admin-settings.php
@@ -20,22 +20,35 @@
 class PCP_Admin_Settings {
 
 	private $pcp_options;
+	private $page_slug = 'primary-cat-for-posts';
 
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
 		add_action( 'admin_init', array( $this, 'init' ) );
 	}
 
+	/**
+	 * Add plugin's options page
+	 * @access public
+	 * @return void
+	 * @since  1.0
+	 */
 	public function add_plugin_page() {
 		add_options_page(
 			'Primary Cat for Posts', // page_title
 			'Primary Cat for Posts', // menu_title
 			'manage_options', // capability
-			'primary-cat-for-posts', // menu_slug
+			$this->page_slug, // menu_slug
 			array( $this, 'admin_page' )
 		);
 	}
 
+	/**
+	 * Render the settings page
+	 * @access public
+	 * @return void
+	 * @since  1.0
+	 */
 	public function admin_page() {
 		$this->pcp_options = PCP::settings(); 
 		PCP_Template_Renderer::render('admin/settings-page.php');		
@@ -52,41 +65,37 @@ class PCP_Admin_Settings {
 			'primary_cat_for_posts_setting_section', // id
 			'General', // title
 			'__return_false', // callback
-			'primary-cat-for-posts' // page
+			$this->page_slug // page
 		);
 
 		add_settings_field(
-			'enabled_post_types', // id
-			'Enable for Post Types', // title
-			array( $this, 'render_enable_for_post_types_setting' ), // callback
-			'primary-cat-for-posts', // page
+			'enabled_taxonomies', // id
+			'Enable Taxonomies', // title
+			array( $this, 'render_enable_taxonomies_setting' ), // callback
+			$this->page_slug, // page
 			'primary_cat_for_posts_setting_section' // section
 		);
 	}
 
 	public function sanitize_input( $input ) {
 		$sanitary_values = array();
-		if ( isset( $input['enabled_post_types'] ) ) {
-			$sanitary_values['enabled_post_types'] = $input['enabled_post_types'];
+		if ( isset( $input['enabled_taxonomies'] ) ) {
+			$sanitary_values['enabled_taxonomies'] = $input['enabled_taxonomies'];
 		}
 
 		return $sanitary_values;
 	}
 
-	public function render_enable_for_post_types_setting() {
-		$post_types = get_post_types(
-			[
-				'public' => true,
-				'show_ui' => true,
-				'publicly_queryable' => true,
-			],
-			'names',
-			'and'
-		);
-
-		PCP_Template_Renderer::render('admin/enabled-post-types-setting.php', [
-			'post_types' => $post_types, 
-			'enabled_post_types' => isset($this->pcp_options['enabled_post_types']) ? $this->pcp_options['enabled_post_types'] : []
+	/**
+	 * Show all taxonomies
+	 * @access public
+	 * @return array
+	 * @since  1.0
+	 */
+	public function render_enable_taxonomies_setting() {
+		PCP_Template_Renderer::render('admin/enabled-taxonomies-setting.php', [
+			'taxonomies' => PCP_Admin::get_all_taxonomies(), 
+			'enabled_taxonomies' => isset($this->pcp_options['enabled_taxonomies']) ? $this->pcp_options['enabled_taxonomies'] : []
 			]
 		);
 	}
