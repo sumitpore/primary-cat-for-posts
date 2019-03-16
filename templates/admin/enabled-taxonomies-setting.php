d1b37363a0a2bc44142b16745f64884f3cb4ddd0
Sumit Pore <sumit.pore@wisdmlabs.com>
2019-03-16 20:34:15 +0530

    Fix Indentation

--- templates/admin/enabled-taxonomies-setting.php
+++ templates/admin/enabled-taxonomies-setting.php
@@ -1,14 +1,14 @@
 <?php if ( $taxonomies ) : ?>
 		<?php foreach ( $taxonomies as $taxonomy_name => $taxonomy ) : ?>
 			<?php
-			printf(
-				'<input type="checkbox" name="pcp_options[enabled_taxonomies][]" id="%s" value="%s" %s><label for="%s">%s</label><br />',
-				esc_attr( $taxonomy_name ),
-				esc_attr( $taxonomy_name ),
-				in_array( $taxonomy_name, $enabled_taxonomies ) ? 'checked' : '',
-				esc_attr( $taxonomy_name ),
-				esc_html( $taxonomy->labels->name )
-			);
+				printf(
+					'<input type="checkbox" name="pcp_options[enabled_taxonomies][]" id="%s" value="%s" %s><label for="%s">%s</label><br />',
+					esc_attr( $taxonomy_name ),
+					esc_attr( $taxonomy_name ),
+					in_array( $taxonomy_name, $enabled_taxonomies ) ? 'checked' : '',
+					esc_attr( $taxonomy_name ),
+					esc_html( $taxonomy->labels->name )
+				);
 			?>
 		<?php endforeach; ?>
 	<?php
