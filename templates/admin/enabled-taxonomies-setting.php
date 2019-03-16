aaaf5e6b8454751d5d95e1988a1f7676bca7eed0
Sumit Pore <sumit.pore@wisdmlabs.com>
2019-03-16 19:23:03 +0530

    Wrap Checkboxes on Settings page in label tag

--- templates/admin/enabled-taxonomies-setting.php
+++ templates/admin/enabled-taxonomies-setting.php
@@ -2,10 +2,11 @@
 		<?php foreach ( $taxonomies as $taxonomy_name => $taxonomy ) : ?>
 			<?php
 			printf(
-				'<input type="checkbox" name="pcp_options[enabled_taxonomies][]" id="%s" value="%s" %s>%s<br />',
+				'<input type="checkbox" name="pcp_options[enabled_taxonomies][]" id="%s" value="%s" %s><label for="%s">%s</label><br />',
 				esc_attr( $taxonomy_name ),
 				esc_attr( $taxonomy_name ),
 				in_array( $taxonomy_name, $enabled_taxonomies ) ? 'checked' : '',
+				esc_attr( $taxonomy_name ),
 				esc_html( $taxonomy->labels->name )
 			);
 			?>
